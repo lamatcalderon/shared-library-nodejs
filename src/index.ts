4ce7710fccb2b3d9993690911f8547b1e70322b5
lamat <lamat@debian-BULLSEYE-live-builder-AMD64>
2024-01-08 18:46:53 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { sum } from "./util/util";
+import { Customer } from "./customers/domain/customer.domain";
 import { CreateCustomerPort } from "./customers/infraestructure/port/in/create-customer.port";
 import { getCreateCustomerPort } from "./util/container";
 
 export {
     sum,
     getCreateCustomerPort,
-    CreateCustomerPort
+    CreateCustomerPort,
+    Customer
 }
